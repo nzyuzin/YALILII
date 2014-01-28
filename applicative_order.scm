59bc4f430370b532a3ee2c21b10e4e63e2de9007
Nikita Zyuzin <nzyuzin93@gmail.com>
2014-01-29 00:04:41 +0400

    add lazy evaluation

---
+++ applicative_order.scm
@@ -0,0 +1,15 @@
+(load "basic_interpreter.scm")
+
+(define input-prompt ";;; M-Eval input:")
+(define output-prompt ";;; M-Eval value:")
+
+(define (driver-loop)
+  (prompt-for-input input-prompt)
+  (let ((input (read)))
+    (let ((output (eval input the-global-environment)))
+      (announce-output output-prompt)
+      (user-print output)))
+  (driver-loop))
+
+(driver-loop)
+
--- basic_interpreter.scm
+++ basic_interpreter.scm
@@ -413,6 +413,8 @@
         (list 'cdr cdr)
         (list 'cons cons)
         (list 'null? null?)
+        (list 'pair? pair?)
+        (list 'equal? equal?)
         (list '+ +)
         (list '- -)
         (list '* *)
@@ -449,31 +451,3 @@
   (apply-in-underlying-scheme
    (primitive-implementation proc) args))
 
-
-
-(define input-prompt ";;; M-Eval input:")
-(define output-prompt ";;; M-Eval value:")
-
-(define (driver-loop)
-  (prompt-for-input input-prompt)
-  (let ((input (read)))
-    (let ((output (eval input the-global-environment)))
-      (announce-output output-prompt)
-      (user-print output)))
-  (driver-loop))
-
-(define (prompt-for-input string)
-  (newline) (newline) (display string) (newline))
-
-(define (announce-output string)
-  (newline) (display string) (newline))
-
-(define (user-print object)
-  (if (compound-procedure? object)
-      (display (list 'compound-procedure
-                     (procedure-parameters object)
-                     (procedure-body object)
-                     '<procedure-env>))
-      (display object)))
-
-(driver-loop)
